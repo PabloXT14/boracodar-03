8c743f8a171906e8e52605bbef3ae61cbc58b248
Pablo Alan <pabloxt14@gmail.com>
2024-04-23 20:02:32 -0300

    chore: initial project setup

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <button className="m-4 inline-block rounded bg-violet-500 px-4 py-2 font-semibold text-violet-50">
+      Buttons & Cursors
+    </button>
+  )
+}
