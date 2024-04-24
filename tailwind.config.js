ad19fc581f4714a98f4fe88e9c64775c53b575cd
Pablo Alan <pabloxt14@gmail.com>
2024-04-24 20:53:14 -0300

    feat: add palate colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ export default {
         roboto: ['Roboto', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
       },
+      colors: {
+        violet: {
+          400: '#9674e5',
+          500: '#8257e5',
+          900: '#1c162c',
+          950: '#130f1e',
+        },
+        zinc: {
+          300: '#d9cdf7',
+          600: '#6a617f',
+          700: '#3c3748',
+          950: '#0b0911',
+        },
+      },
     },
   },
   plugins: [],
