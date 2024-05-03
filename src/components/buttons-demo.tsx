9ee7febc1d4755b14d75fbe0480bfd02db549e48
Pablo Alan <pabloxt14@gmail.com>
2024-05-03 20:50:37 -0300

    feat: animate sidebar menu

--- src/components/buttons-demo.tsx
+++ src/components/buttons-demo.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import { motion } from 'framer-motion'
 import { LiaPuzzlePieceSolid } from 'react-icons/lia'
 import { Button } from './button'
 
@@ -15,24 +16,32 @@ export const ButtonsDemo = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
-        <Dialog.Content className="fixed bottom-0 right-0 top-0 w-fit max-w-[406px] bg-violet-950 px-11 py-28">
-          <Dialog.Title className="mb-2 font-inter text-[32px] font-bold leading-none">
-            Teste os botões
-          </Dialog.Title>
-          <Dialog.Description className="mb-16 text-lg opacity-65">
-            Interaja com os botões e observe a mudança de aparência e de
-            cursores
-          </Dialog.Description>
+        <Dialog.Content asChild>
+          <motion.div
+            className="fixed bottom-0 right-0 top-0 w-fit max-w-[406px] bg-violet-950 px-11 py-28"
+            initial={{ x: '100%' }}
+            whileInView={{ x: 0 }}
+            exit={{ x: '100%' }}
+            transition={{ duration: 0.3 }}
+          >
+            <Dialog.Title className="mb-2 font-inter text-[32px] font-bold leading-none">
+              Teste os botões
+            </Dialog.Title>
+            <Dialog.Description className="mb-16 text-lg opacity-65">
+              Interaja com os botões e observe a mudança de aparência e de
+              cursores
+            </Dialog.Description>
 
-          <div className="flex w-full flex-col gap-8">
-            <Button>Interaja comigo</Button>
-            <Button variant="secondary" disabled>
-              Interaja comigo
-            </Button>
-            <Button variant="tertiary" state="movable">
-              Interaja comigo
-            </Button>
-          </div>
+            <div className="flex w-full flex-col gap-8">
+              <Button>Interaja comigo</Button>
+              <Button variant="secondary" disabled>
+                Interaja comigo
+              </Button>
+              <Button variant="tertiary" state="movable">
+                Interaja comigo
+              </Button>
+            </div>
+          </motion.div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
