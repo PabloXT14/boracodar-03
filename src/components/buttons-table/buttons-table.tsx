08cd66c61420b25fd1be53c3c8722487b81ed940
Pablo Alan <pabloxt14@gmail.com>
2024-05-05 19:26:16 -0300

    feat: animate buttons-table

--- src/components/buttons-table/buttons-table.tsx
+++ src/components/buttons-table/buttons-table.tsx
@@ -7,10 +7,27 @@ import { CursorDefault } from '../../assets/icons/cursor-default'
 import { CursorNotAllowed } from '../../assets/icons/cursor-not-allowed'
 import { CursorLoading } from '../../assets/icons/cursor-loading'
 import { CursorMove } from '../../assets/icons/cursor-move'
+import { motion } from 'framer-motion'
 
 export const ButtonsTable = () => {
+  const buttonAnimation = {
+    initial: { opacity: 0, x: -20 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -20 },
+  }
+
+  const tableTransitionDuration = 0.5
+  const buttonDefaultDelay = tableTransitionDuration
+  const tableLinesDelay = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+
   return (
-    <div className="overflow-hidden rounded-lg border-b-[6px] border-r-[7px] border-b-violet-900 border-r-violet-900">
+    <motion.div
+      className="overflow-hidden rounded-lg border-b-[6px] border-r-[7px] border-b-violet-900 border-r-violet-900"
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 100 }}
+      transition={{ duration: tableTransitionDuration }}
+    >
       <table className="w-full">
         <thead>
           <tr>
@@ -25,118 +42,310 @@ export const ButtonsTable = () => {
           <TableRow className="[&>td]:pt-8">
             <TableCellLeft>Default</TableCellLeft>
             <TableCell>
-              <Button>Default Primary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[0],
+                }}
+              >
+                <Button>Default Primary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="secondary">Default Secondary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[0],
+                }}
+              >
+                <Button variant="secondary">Default Secondary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="tertiary">Default Tertiary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[0],
+                }}
+              >
+                <Button variant="tertiary">Default Tertiary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <CursorDefault className="mx-auto" width={20} height={20} />
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[0],
+                }}
+              >
+                <CursorDefault className="mx-auto" width={20} height={20} />
+              </motion.div>
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCellLeft>Hover</TableCellLeft>
             <TableCell>
-              <Button state="hover">Hover Primary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[1],
+                }}
+              >
+                <Button state="hover">Hover Primary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="secondary" state="hover">
-                Hover Secondary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[1],
+                }}
+              >
+                <Button variant="secondary" state="hover">
+                  Hover Secondary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="tertiary">Hover Tertiary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[1],
+                }}
+              >
+                <Button variant="tertiary">Hover Tertiary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <CursorDefault className="mx-auto" width={20} height={20} />
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[1],
+                }}
+              >
+                <CursorDefault className="mx-auto" width={20} height={20} />
+              </motion.div>
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCellLeft>Focus</TableCellLeft>
             <TableCell>
-              <Button state="focus">Focus Primary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[2],
+                }}
+              >
+                <Button state="focus">Focus Primary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="secondary" state="focus">
-                Focus Secondary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[2],
+                }}
+              >
+                <Button variant="secondary" state="focus">
+                  Focus Secondary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="tertiary" state="focus">
-                Focus Tertiary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[2],
+                }}
+              >
+                <Button variant="tertiary" state="focus">
+                  Focus Tertiary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <CursorDefault className="mx-auto" width={20} height={20} />
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[2],
+                }}
+              >
+                <CursorDefault className="mx-auto" width={20} height={20} />
+              </motion.div>
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCellLeft>Disabled</TableCellLeft>
             <TableCell>
-              <Button disabled>Disabled Primary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[3],
+                }}
+              >
+                <Button disabled>Disabled Primary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="secondary" disabled>
-                Disabled Secondary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[3],
+                }}
+              >
+                <Button variant="secondary" disabled>
+                  Disabled Secondary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="tertiary" disabled>
-                Disabled Tertiary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[3],
+                }}
+              >
+                <Button variant="tertiary" disabled>
+                  Disabled Tertiary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <CursorNotAllowed className="mx-auto" width={20} height={20} />
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[3],
+                }}
+              >
+                <CursorNotAllowed className="mx-auto" width={20} height={20} />
+              </motion.div>
             </TableCell>
           </TableRow>
 
           <TableRow>
             <TableCellLeft>Loading</TableCellLeft>
             <TableCell>
-              <Button state="loading">Loading Primary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[4],
+                }}
+              >
+                <Button state="loading">Loading Primary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="secondary" state="loading">
-                Loading Secondary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[4],
+                }}
+              >
+                <Button variant="secondary" state="loading">
+                  Loading Secondary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="tertiary" state="loading">
-                Loading Tertiary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[4],
+                }}
+              >
+                <Button variant="tertiary" state="loading">
+                  Loading Tertiary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <CursorLoading className="mx-auto" width={20} height={20} />
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[4],
+                }}
+              >
+                <CursorLoading className="mx-auto" width={20} height={20} />
+              </motion.div>
             </TableCell>
           </TableRow>
 
           <TableRow className="[&>td]:pb-8">
             <TableCellLeft>Movable</TableCellLeft>
             <TableCell>
-              <Button state="movable">Movable Primary</Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[5],
+                }}
+              >
+                <Button state="movable">Movable Primary</Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="secondary" state="movable">
-                Movable Secondary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[5],
+                }}
+              >
+                <Button variant="secondary" state="movable">
+                  Movable Secondary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <Button variant="tertiary" state="movable">
-                Movable Tertiary
-              </Button>
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[5],
+                }}
+              >
+                <Button variant="tertiary" state="movable">
+                  Movable Tertiary
+                </Button>
+              </motion.div>
             </TableCell>
             <TableCell>
-              <CursorMove className="mx-auto" width={20} height={20} />
+              <motion.div
+                {...buttonAnimation}
+                transition={{
+                  duration: 0.2,
+                  delay: buttonDefaultDelay + tableLinesDelay[5],
+                }}
+              >
+                <CursorMove className="mx-auto" width={20} height={20} />
+              </motion.div>
             </TableCell>
           </TableRow>
         </tbody>
       </table>
-    </div>
+    </motion.div>
   )
 }
