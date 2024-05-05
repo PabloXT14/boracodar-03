65ddf655449cfae19949f419b436fe2aafb6899c
Pablo Alan <pabloxt14@gmail.com>
2024-05-05 18:52:19 -0300

    feat: animate page-description

--- src/components/page-description.tsx
+++ src/components/page-description.tsx
@@ -1,12 +1,30 @@
+import { motion } from 'framer-motion'
+
 export const PageDescription = () => {
+  const animProps = {
+    initial: { opacity: 0, x: -100 },
+    whileInView: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -100 },
+  }
+
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-[32px] font-bold leading-none">Tipos de botão</h1>
-      <p className="text-lg font-normal opacity-65">
+      <motion.h1
+        className="text-[32px] font-bold leading-none"
+        {...animProps}
+        transition={{ duration: 0.5 }}
+      >
+        Tipos de botão
+      </motion.h1>
+      <motion.p
+        className="text-lg font-normal opacity-65"
+        {...animProps}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
         Dentro de um layout, botões servem para destacar ações importantes a
         serem tomadas. <br />
         Acompanhe abaixo um exemplo de tipos e propriedades.
-      </p>
+      </motion.p>
     </div>
   )
 }
